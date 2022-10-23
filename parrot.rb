74916661a0d088546fc65c6a726488eda7069ecf
rebecca njeri <rebecca.njeri@student.moringaschool.com>
2022-10-23 21:04:32 +0300

    complete assignment

--- parrot.rb
+++ parrot.rb
@@ -1,2 +1,13 @@
 # Create method `parrot` that outputs a given phrase and
+puts "Squawk!"
+
+def parrot()
+    puts "Squawk!"
+end
+
+def parrot(sound ="")
+    puts sound =="" ? "Squawk!": sound
+    sound =="" ? "Squawk!": sound
+end
+"Squawk!"
 # returns the phrase
